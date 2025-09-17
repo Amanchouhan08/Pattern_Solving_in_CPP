5baae4c7835d71a38a1120eed3931a660d4600b0
Aman Chouhan <ak6927887@gmail.com>
2025-09-17 23:21:26 +0530

    Add files via upload

---
+++ Level-2/p4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>;
+using namespace std;
+int main(){
+for(int i=1;i<=5;i++){
+    for(int j=i;j>0;j--){
+        cout<<j<<" ";
+    }
+    cout<<endl;
+}
+return 0;
+}
+
+/*
+1 
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+*/
